64705b7b30da8b38a72885c312bd5b32cdcb5967
gunja24bothra <gunjan.bothra@driscolls.com>
2020-12-11 12:19:38 +0530

    Updates

---
+++ precache-manifest.61aa50ff61e26523ab0edd53e298390b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "972edd08a744ecae948e7bc4c5d6d24a",
+    "url": "/%7Brepo-name%7D/index.html"
+  },
+  {
+    "revision": "d22623514120d147ed42",
+    "url": "/%7Brepo-name%7D/static/js/2.d6d7e320.chunk.js"
+  },
+  {
+    "revision": "543700d7b10f66c8415fcb490fedbe86",
+    "url": "/%7Brepo-name%7D/static/js/2.d6d7e320.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "debd9d7aec43dfbfea9d",
+    "url": "/%7Brepo-name%7D/static/js/main.ae479890.chunk.js"
+  },
+  {
+    "revision": "63ea531027fd1a4bf0e9",
+    "url": "/%7Brepo-name%7D/static/js/runtime-main.0b860f0e.js"
+  }
+]);
